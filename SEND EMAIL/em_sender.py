3fc58d4e54ffcbb000bb8de7eb611f0b102abc0a
harshkakaria <harshkakaria1@gmail.com>
2020-11-28 00:39:18 +0530

    new projrct

---
+++ SEND EMAIL/em_sender.py
@@ -0,0 +1,14 @@
+import stdiomask
+import smtplib
+
+sender_email = str(input("enter sender's email ID: "))
+rec_email = str(input("enter reciver's email ID: "))
+password =stdiomask.getpass(prompt='PLS ENTER UR PASSWORD: ')
+message = str(input("what is the msg u want to send:"))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender_email, password)
+print("Login success")
+server.sendmail(sender_email, rec_email, message)
+print("Email has been sent to ", rec_email)
